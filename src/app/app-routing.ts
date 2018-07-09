3ddb5b670c49d42df3a978026eddb32f718e7909
ylxz0073 <jiapengtao@yahoo.com>
2018-07-09 16:33:35 -0700

    initial commit

---
+++ src/app/app-routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { WhiteBoardComponent} from './white-board/white-board.component';
+import {CourseViewerComponent} from './course-viewer/course-viewer.component';
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: WhiteBoardComponent},
+  { path: 'course/:courseId', component: CourseViewerComponent},
+  { path: 'course/:courseId/module/:moduleId', component: CourseViewerComponent},
+  { path: 'course/:courseId/module/:moduleId/lesson/:lessonId', component: CourseViewerComponent},
+  { path: '**', component: WhiteBoardComponent}
+];
+export const routing = RouterModule.forRoot(appRoutes);
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {FormsModule} from '@angular/forms';
+import {CourseNavigatorServiceClient} from './services/course-navigator.service.client';
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+import { WhiteBoardComponent } from './white-board/white-board.component';
+import { CourseGridComponent } from './course-grid/course-grid.component';
+import {CourseServiceClient} from './services/course.service.client';
+import {routing} from './app-routing';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import {ModuleServiceClient} from './services/module.service.client';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import {LessonServiceClient} from './services/lesson.service.client';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    CourseNavigatorComponent,
+    WhiteBoardComponent,
+    CourseGridComponent,
+    CourseViewerComponent,
+    ModuleListComponent,
+    LessonTabsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing
+  ],
+  providers: [
+    CourseNavigatorServiceClient,
+    CourseServiceClient,
+    ModuleServiceClient,
+    LessonServiceClient
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
